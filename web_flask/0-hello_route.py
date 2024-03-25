b8e9684537a1289a37b8c80341562ffdb5165528
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-03-25 00:49:08 +0000

    hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """A Flask web application with its route ('/'):
-    display “Hello HBNB!"""
+    display “Hello HBNB!
+   => Current file: 0-hello_route.py"""
+
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return "Hello HBNB!"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
 
